570314ffd0e4716956c2131222514a9718bb8596
mahmmad-khamar-shaikh <shahid13384@hotmail.com>
2018-08-19 19:02:00 +0530

    Added RealTime Database instance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {AngularFireModule} from 'angularfire2';
-import {AngularFireAuthModule} from 'angularfire2/auth';
-import {AngularFireDatabaseModule} from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 
 import { AppComponent } from './app.component';
@@ -20,6 +20,9 @@ import { DashbaordComponent } from './dashbaord/dashbaord.component';
 import { ManageDashboardComponent } from './manage-dashboard/manage-dashboard.component';
 import { BookBoardComponent } from './book-board/book-board.component';
 import { SignupComponent } from './shared/signup/signup.component';
+import { AngularFirestore } from '../../node_modules/angularfire2/firestore';
+/** Material component */
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
@@ -38,13 +41,14 @@ import { SignupComponent } from './shared/signup/signup.component';
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
+    MatSlideToggleModule,
     AppMaterialModule,
     AngularFireDatabaseModule,
     AngularFireModule.initializeApp(environment.firebase, 'angular-auth-firebase'),
     AngularFireAuthModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [AuthService],
+  providers: [AuthService, AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/book-board/book-board.component.ts
+++ src/app/book-board/book-board.component.ts
@@ -2,126 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material';
 import { IBook } from '../types/book.interfacce';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { AngularFirestoreCollection, AngularFirestoreDocument, AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 
-const ELEMENT_DATA: IBook[] = [
-  {
-    Id: 1, assignee: 'Shaikh',
-    title: 'The proagmatic programmer', isAvailable: false, owner: 'Amol Jadhav', author: 'Andy Hunt, Dave Thomas'
-  },
-  {
-    Id: 2, assignee: '',
-    title: 'The Art of Agile Development', isAvailable: true, owner: 'Amol Jadhav', author: 'James Shore', subTitle: ''
-  },
-  {
-    Id: 3, assignee: 'ravi',
-    title: 'Clean Code', isAvailable: false, owner: 'Amol Jadhav', author: 'Robert Cecil Martin'
-  },
-  {
-    Id: 4, assignee: '',
-    title: 'Clean Architecture', isAvailable: true, owner: 'Amol Jadhav', author: 'Robert Cecil Martin'
-  }
-  ,
-  {
-    Id: 5, assignee: '',
-    title: 'Head First JavaScript Programming', isAvailable: true, owner: 'Amol Jadhav', author: 'Eric Freeman, Elisabeth Freeman'
-  },
-  {
-    Id: 6, assignee: '',
-    title: 'Spring in Action', isAvailable: true, owner: 'Amol Jadhav', author: 'Craig Walls'
-  }
-  ,
-  {
-    Id: 7, assignee: '',
-    title: 'Developing Backbone.js Application', isAvailable: true, owner: 'Amol Jadhav', author: 'Addy Osmani',
-    subTitle: 'Building Better JavaScript Applications'
-  },
-  {
-    Id: 2, assignee: '',
-    title: 'JUnit in Action', isAvailable: true, owner: 'Amol Jadhav', author: 'Vincent Massol, Ted Husted', subTitle: ''
-  },
-  {
-    Id: 8, assignee: '',
-    title: 'AngularJS', isAvailable: true, owner: 'Amol Jadhav', author: 'Green & Seshadri'
-  },
-  {
-    Id: 9, assignee: '',
-    title: 'Clean Code Architecture', isAvailable: true, owner: 'Amol Jadhav', author: 'Rob Martin'
-  }
-  ,
-  {
-    Id: 10, assignee: '',
-    title: 'Selenium Testing Tools Cookbook', isAvailable: true, owner: 'Amol Jadhav', author: 'Unmesh Gundecha'
-  },
-  {
-    Id: 11, assignee: 'Vaibhav',
-    title: 'You Dont Know JS', isAvailable: true, owner: 'Amol Jadhav', author: 'Kyle Simpson',
-    subTitle: 'Async & Performance'
-  }
-  ,
-  {
-    Id: 12, assignee: '',
-    title: 'You Dont Know JS', isAvailable: true, owner: 'Amol Jadhav', author: 'Kyle Simpson', subTitle: 'Up & Going'
-  }
-  ,
-  {
-    Id: 13, assignee: '',
-    title: 'NoSQL Distilled', isAvailable: true, owner: 'Amol Jadhav', author: 'Martin Fowler, Pramod J. Sadalage',
-    subTitle: 'A Brief Guide to the Emerging World of Polyglot Persistence'
-  }
-
-  ,
-  {
-    Id: 14, assignee: '',
-    title: 'Spring Microservices', isAvailable: true, owner: 'Amol Jadhav', author: 'Rajesh RV', subTitle: ''
-  }
-  ,
-  {
-    Id: 15, assignee: '',
-    title: 'Head First Design Patterns', isAvailable: true, owner: 'Amol Jadhav',
-    author: 'Elisabeth Freeman and Kathy Sierra', subTitle: 'Up & Going'
-  }
-  ,
-  {
-    Id: 16, assignee: '',
-    title: 'Bootstrap', isAvailable: true, owner: 'Amol Jadhav', author: 'Jake Spurlock', subTitle: 'Responsive Web Development'
-  }
-  ,
-  {
-    Id: 17, assignee: '',
-    title: 'Soft Skills', isAvailable: true, owner: 'Amol Jadhav', author: 'John Z. Sonmez', subTitle: 'The Software Developers Life Manual'
-  }
-  ,
-  {
-    Id: 18, assignee: '',
-    title: 'Drive', isAvailable: true, owner: 'Amol Jadhav',
-    author: 'Daniel H. Pink', subTitle: 'The Surprising Truth About What Motivates Us'
-  },
-  {
-    Id: 19, assignee: '',
-    title: 'Out of the Crisis', isAvailable: true, owner: 'Amol Jadhav',
-    author: 'W. Edwards Deming', subTitle: ''
-  },
-  {
-    Id: 20, assignee: '',
-    title: 'Blockchain Revolution', isAvailable: true, owner: 'Amol Jadhav',
-    author: 'Don Tapscott, Alex Tapscott', subTitle: 'How the Technology Behind Bitcoin Is Changing Money'
-  },
-  {
-    Id: 21, assignee: '',
-    title: 'The Machine That Changed the World', isAvailable: true, owner: 'Amol Jadhav',
-    author: ' James P. Womack', subTitle: ''
-  },
-  {
-    Id: 22, assignee: '',
-    title: 'Leadership by the Book', isAvailable: true, owner: 'Amol Jadhav',
-    author: 'Kenneth H. Blanchard, Bill Hybels, PHIL HODGES', subTitle: 'Tools to Transform Your Workplace'
-  }
-
-
-
-];
 
 @Component({
   selector: 'app-book-board',
@@ -129,8 +15,8 @@ const ELEMENT_DATA: IBook[] = [
   styleUrls: ['./book-board.component.css'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
-      state('expanded', style({height: '*'})),
+      state('collapsed', style({ height: '0px', minHeight: '0', display: 'none' })),
+      state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
@@ -139,11 +25,19 @@ export class BookBoardComponent implements OnInit {
   public myControl = new FormControl();
   options: string[] = ['One', 'Two', 'Three'];
   displayedColumns: string[] = ['Id', 'title', 'isAvailable'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  dataSource: any; // MatTableDataSource<IBook>;
   expandedElement: IBook;
-  constructor() { }
+  booksCollection: AngularFirestoreCollection<IBook>;
+  books: Observable<IBook[]>;
+  constructor(private _asfServiceReference: AngularFirestore) { }
 
   ngOnInit() {
+    this.booksCollection = this._asfServiceReference.collection('books', ref => ref.orderBy('Id'));
+    this.books = this.booksCollection.valueChanges();
+    this.books.subscribe(data => {
+      console.log('booksData', data);
+      this.dataSource = new MatTableDataSource(data);
+    });
   }
 
 
